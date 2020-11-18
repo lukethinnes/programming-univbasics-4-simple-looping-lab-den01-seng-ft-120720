18eae22949d3e68d44dfb247a016b9a3eb6bcf7c
Luke Thinnes <lukethinnes@yahoo.com>
2020-11-18 20:22:18 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,1 +1,20 @@
 # Write your methods here
+def loop_message_five_times(str)
+  counter=1
+  while counter < 1 do
+      puts str
+      counter++
+  end
+end
+
+def loop_message_n_times
+
+end
+
+def output_array
+
+end
+
+def return_string_array
+
+end
