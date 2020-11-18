a84bdc17973d25a2388ff50dfa43296f2a529b9a
Luke Thinnes <lukethinnes@yahoo.com>
2020-11-18 20:26:48 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -24,6 +24,10 @@ def output_array(array)
   end
 end
 
-def return_string_array
-
+def return_string_array(array)
+  counter=0
+  while counter < array.length do
+    return array[counter]
+    counter+=1
+  end
 end
